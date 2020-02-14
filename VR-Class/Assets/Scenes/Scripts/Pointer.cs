291602e6360edad9b725581774a514c55de8d694
Kaven arango <43826797+KavenArango@users.noreply.github.com>
2020-02-14 01:08:21 -0500

    added camrea rig and working on raycast for main menu

---
+++ VR-Class/Assets/Scenes/Scripts/Pointer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+    public float m_DefaultLength = 5.0f;
+    public GameObject m_Dot;
+    public VRInputModule = InputModule;
+    
+
+
+
+    private RaycastHit CreateRayCast(float length)
+    {
+        RaycastHit hit;
+        return hit;
+    }
+
+
+
+    private void Awake()
+    {
+        
+    }
+
+
+    private void UpdateLine()
+    {
+
+    }
+
+
+    private void Update()
+    {
+        
+    }
+}
